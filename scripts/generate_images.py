bda02e221119cd65a5fc94ba10864c326cb10752
realantonvoronov <avoronov3797@gmail.com>
2024-05-18 15:56:05 +0300

    nirvana fix for path to calibrated ckpt

--- scripts/generate_images.py
+++ scripts/generate_images.py
@@ -196,7 +196,7 @@ def generate_with_quantized_sdxl(pipe, prompt, num_images_per_prompt=1, output_t
 
 def get_checkpoint_path(init_path):
     inner_folder = os.listdir(os.path.join(init_path, 'auto_logs'))[0]
-    res_path = os.path.join(init_path, 'auto_logs', inner_folder, 'ckpt.pth')
+    res_path = os.path.join(init_path, 'auto_logs', inner_folder, 'pydl_inner_cube', 'ckpt.pth')
     print(res_path)
     return res_path
 
